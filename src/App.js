0dae7f771023d42effab92bdae5061367198ab1d
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-12 21:54:13 -0700

    CSS Syled

--- src/App.js
+++ src/App.js
@@ -24,8 +24,9 @@ class App extends React.Component {
             {
               this.state.monitors.map((monitor) => {
                 return <div className="item">
-                  <img className="image" src={monitor.image} alt={monitor.model} />
                   <ul>
+                    <li><img className="image" src={monitor.image} alt={monitor.model} /></li>
+                  
                     <li><h2>{monitor.brand}</h2></li>
                     <li><h3>{monitor.model}</h3></li>
                     <li>Cool factor of {monitor.cool_factor}</li>
